1a0a0eb6b271a1169ef73a8a29681f0b0584f08a
Patrick Faramaz <faramazpat@gmail.com>
2018-10-29 12:48:44 +0100

    decompose ssr function

--- src/ssr.js
+++ src/ssr.js
@@ -22,7 +22,8 @@ export const getRenderType = (
   return type;
 };
 
-export const getBrowser = async () =>
+// launch browser
+const launch = async (): puppeteer.Browser =>
   await puppeteer.launch({
     headless: true,
     args: [
@@ -35,6 +36,56 @@ export const getBrowser = async () =>
     ],
   });
 
+// get page for navigate
+export const getPage = async (): puppeteer.Page => {
+  if (!browserWSEndpoint) {
+    const browserAlreadyStarted = await launch();
+    browserWSEndpoint = await browserAlreadyStarted.wsEndpoint();
+  }
+  const browser = await puppeteer.connect({browserWSEndpoint});
+  // new page
+  const page = await browser.newPage();
+
+  return page;
+};
+
+const filterPageRequest = async (page: puppeteer.Page): Promise<void> => {
+  // 1. Intercept network requests.
+  await page.setRequestInterception(true);
+  page.on('request', req => {
+    const whitelist = ['document', 'script', 'xhr', 'fetch'];
+    if (!whitelist.includes(req.resourceType())) {
+      return req.abort();
+    }
+    req.continue();
+  });
+};
+
+// get page format by type
+const getResult = async (
+  type: string,
+  page: puppeteer.Page,
+): Promise<string> => {
+  switch (type) {
+    case 'html':
+      return await page.content();
+    case 'png':
+      return await page.screenshot({
+        type,
+        fullPage: true,
+      });
+    case 'jpeg':
+      return await page.screenshot({
+        type,
+        fullPage: true,
+      });
+    case 'pdf':
+      return await page.pdf();
+    default:
+      return await page.content();
+  }
+};
+
 export async function ssr(url: string, renderType: string): Promise<string> {
   const type: string = getRenderType(renderType, renderTypeOptions);
   const keyCache: string = `${url}${type}`;
@@ -43,64 +94,17 @@ export async function ssr(url: string, renderType: string): Promise<string> {
   if (cacheUrl !== undefined && cacheUrl !== null) {
     return cacheUrl;
   }
-  if (!browserWSEndpoint) {
-    const browserAlreadyStarted = await getBrowser();
-    browserWSEndpoint = await browserAlreadyStarted.wsEndpoint();
-  }
-  // get browser
-  const browser = await puppeteer.connect({browserWSEndpoint});
-
-  // new page
-  const page = await browser.newPage();
-
-  // 1. Intercept network requests.
-  await page.setRequestInterception(true);
-  page.on('request', req => {
-    // 2. Ignore requests for resources that don't produce DOM
-    // (images, stylesheets, media).
-
-    const whitelist = ['document', 'script', 'xhr', 'fetch'];
-    if (!whitelist.includes(req.resourceType())) {
-      return req.abort();
-    }
-    // 3. Pass through all other requests.
-    req.continue();
-  });
-
+  // launch browser and get page
+  const page = await getPage();
+  filterPageRequest(page);
   try {
-    // networkidle0 waits for the network to be idle (no requests for 500ms).
-    // The page's JS has likely produced markup by this point, but wait longer
-    // if your site lazy loads, etc.
     await page.goto(url, {waitUntil: 'networkidle0'});
   } catch (err) {
     throw err;
-    // console.error(err);
   }
 
-  let result = '';
-  switch (type) {
-    case 'html':
-      result = await page.content();
-      break;
-    case 'png':
-      result = await page.screenshot({
-        type,
-        fullPage: true,
-      });
-      break;
-    case 'jpeg':
-      result = await page.screenshot({
-        type,
-        fullPage: true,
-      });
-      break;
-    case 'pdf':
-      result = await page.pdf();
-      break;
-    default:
-      result = await page.content();
-      break;
-  }
+  const result = await getResult(type, page);
+
   await page.close();
   ssrCache.set(keyCache, result); // cache rendered page.
 
